ad939d7c20325753c2d41de9dcc46d81e3228675
BraynerB <brayner.barragann@gmail.com>
2021-03-20 11:57:42 -0500

    solo funciona con numeros de 1 cifra y con una sola operacion ejemplo 2*5

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { isNumber } from 'util';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,5 +8,65 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  n1 = 1;
+  n2 = 2;
+  n3 = 3;
+  n4 = 4;
+  n5 = 5;
+  n6 = 6;
+  n7 = 7;
+  n8 = 8;
+  n9 = 9;
+  n0 = 0;
+  suma = '+';
+  resta = '-';
+  multiplicacion = '*';
+  division = '/';
+
+  escribe = '';
+  resultado = 0;
+  nu1 = 0;
+  nu2 = 0;
+  operador = '';
+  s= 0;
+  escribir(c){
+    if(isNumber(c) && this.s == 0){
+      this.nu1=c;
+    }
+    if( this.s == 1){
+      this.operador = c;
+    }
+    if(isNumber(c) && this.s == 2){
+      this.nu2 = c;
+    }
+
+    if (this.escribe==''){
+      this.escribe = c;
+    }else{
+      this.escribe= this.escribe + c;
+    }
+    this.s = this.s + 1;
+  }
+  calcular(){
+    if(this.operador == '+'){
+      
+      this.resultado = this.nu1+this.nu2;
+    }
+    if(this.operador == '-'){
+      
+      this.resultado = this.nu1-this.nu2;
+    }
+    if(this.operador == '/'){
+      
+      this.resultado = this.nu1/this.nu2;
+    }
+    if(this.operador == '*'){
+      
+      this.resultado = this.nu1*this.nu2;
+    }
+    this.s= 0;
+    this.escribe = '';
+  }
+
 
 }
